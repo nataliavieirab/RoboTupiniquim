40b59bedd4a3de7bb2220f9f5375d26fbbfee28a
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-24 17:56:48 -0300

    First Commit

---
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RoboTupiniquim.ConsoleApp;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+
+    Console.WriteLine("===========================");
+    Console.WriteLine("----- Robô Tupiniquim -----");
+    Console.WriteLine("===========================");
+    Console.WriteLine();
+
+  }
+}
