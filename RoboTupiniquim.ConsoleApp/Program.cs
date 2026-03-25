8fbe831871425fd42e3ae06a189d16607dc04d50
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-25 15:51:01 -0300

    refactor: alterações nas orientações de input

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -7,31 +7,31 @@ class Program
 
     ShowHeader();
 
-    Console.Write("Digite o valor da posição X: ");
+    Console.Write("• Digite o valor da posição X: ");
     int x = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Digite o valor da posição Y: ");
+    Console.Write("• Digite o valor da posição Y: ");
     int y = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Digite a orientação: ");
+    Console.Write("• Digite a orientação: ");
     string? direction = Console.ReadLine()?.ToUpper();
 
-    Console.Write("Digite a sequência de comandos: ");
+    Console.Write("• Digite a sequência de comandos: ");
     string? commands = Console.ReadLine()?.ToUpper();
 
     Robot robot = new Robot(x, y, direction!);
 
     robot.ExecuteCommands(commands!);
 
-    Console.WriteLine($"\nPosição alcançada: {robot.GetFinalPosition()}");
+    Console.WriteLine($"\n>> Posição alcançada: {robot.GetFinalPosition()}");
     Console.ReadLine();
   }
 
   static void ShowHeader()
   {
-    Console.WriteLine("===========================");
-    Console.WriteLine("----- Robô Tupiniquim -----");
-    Console.WriteLine("===========================");
+    Console.WriteLine("================================");
+    Console.WriteLine("------- Robô Tupiniquim --------");
+    Console.WriteLine("================================");
     Console.WriteLine();
   }
 }
